3755ed1a112b53cc906dc69cf8000728129d8290
Ian Spiro <ispiro@gmail.com>
2012-03-29 17:12:23 -0400

    First commit

---
+++ proxy.php
@@ -0,0 +1,9 @@
+<?php
+     $content = file_get_contents($_GET['url']);
+ 
+     if ($content !== false) {   
+          echo($content);
+     } else {  
+          // there was an error
+     }
+?>
