3fece526d47b4622b842a84ab0129f16d59fec37
Matt Tierney <tierney@cs.nyu.edu>
2012-04-06 08:41:14 -0400

    WIP desktop

--- desktop/code.py
+++ desktop/code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from Crypto.Cipher import AES
-from PIL import Image
+from PIL import Image, ImageDraw
 from tempfile import NamedTemporaryFile
 import PIL
 import base64
@@ -42,53 +42,142 @@ class Cipher(object):
 
 class SeeMeNotImage(threading.Thread):
   image = None
+  b64_image = None
+  rgb_image = None
 
-  def __init__(self, image_path, width=None, height=None, quality=None):
+  def __init__(self, image_path, scale, quality=None, block_size=None):
     self.image_path = image_path
-    self.width = width
-    self.height = height
+    self.scale = scale
     self.quality = quality
+    self.block_size = block_size
     threading.Thread.__init__(self)
 
-  def rescale(self, width, height):
-    self.image = self.image.resize((width, height))
+  def _get_wrgbk(self, block):
+    upper_thresh = 150
+    lower_thresh = 50
+    count = 0
+    rt = 0
+    gt = 0
+    bt = 0
+
+    block_len = len(list(block.getdata()))
+    for i in range(0, block_len, 4):
+      rt += list(block.getdata())[i][0]
+      gt += list(block.getdata())[i][1]
+      bt += list(block.getdata())[i][2]
+      count += 1
+    r = rt / count
+    g = gt / count
+    b = bt / count
+
+    if (r > upper_thresh and b > upper_thresh and g > upper_thresh): return 0
+    if (r < lower_thresh and g < lower_thresh and b < lower_thresh): return 4
+
+    if ( r > g and r > b): return 1
+    if ( g > r and g > b): return 2
+    if ( b > r and b > g): return 3
+
+    return -1
+
+
+  def rescale(self):
+    width, height = self.image.size
+    self.image = self.image.resize(
+      (int(width * self.scale), int(height * self.scale)))
 
   def requality(self, quality):
     with NamedTemporaryFile() as fh:
-      image_name = fh.name + '.jpg'
-      self.image.save(image_name, quality=quality)
-      self.image = Image.open(image_name)
+      self.image_path = fh.name + '.jpg'
+      self.image.save(self.image_path, quality=quality)
+      self.image = Image.open(self.image_path)
 
   def encode(self):
-    return base64.b64encode(self.image.tostring())
+    with NamedTemporaryFile() as fh:
+      image_path = fh.name + '.jpg'
+      self.image.save(image_path)
+    with open(image_path, 'rb') as fh:
+      self.b64_image = base64.b64encode(fh.read())
 
-  def encrypt(self):
-    pass
+  def encrypt(self, password):
+    c = Cipher(password)
+    b64encoded = c.encode(self.b64_image)
+
+    width, length = self.image.size
+    TARGET_WIDTH = width
+
+    hex_data = binascii.hexlify(base64.b64decode(b64encoded))
+    num_data = len(hex_data)
+    width = int(TARGET_WIDTH / (self.block_size * 2));
+    height = int(math.ceil(num_data / width))
+
+    self.rgb_image = Image.new('RGBA', (width * self.block_size * 2,
+                                        height * self.block_size))
+
+    colors = [(255,255,255), (255,0,0), (0,255,0), (0,0,255)]
+    for i, hex_datum in enumerate(hex_data):
+      hex_val = int(hex_datum, 16)
+      base4_1 = int(math.floor(hex_val / 4.0))
+      base4_0 = int(hex_val - (base4_1 * 4))
+      y_coord = int(math.floor(i / width))
+      x_coord = int(i - (y_coord * width))
+
+      draw = ImageDraw.Draw(self.rgb_image)
+      base4_0_x = int(x_coord * self.block_size * 2)
+      base4_0_y = int(y_coord * self.block_size)
+      draw.rectangle([(base4_0_x, base4_0_y),
+                      (base4_0_x + self.block_size, base4_0_y + self.block_size)],
+                     fill=colors[base4_0])
+      base4_1_x = (x_coord * self.block_size * 2) + self.block_size
+      base4_1_y = y_coord * self.block_size
+      draw.rectangle([(base4_1_x, base4_1_y),
+                      (base4_1_x + self.block_size, base4_1_y + self.block_size)],
+                     fill=colors[base4_1])
+
+    filename = 'rgb.jpg'
+    self.rgb_image.save(filename)
+    return filename
+
+  def extract_rgb(self):
+    # self.block_size
+    width, height = self.rgb_image.size
+    im = Image.new('RGBA', (width,height))
+    hex_string = ''
+    count = 0
+    for y in range(0, height, self.block_size):
+      for x in range(0, width, self.block_size * 2):
+
+        block0 = self.rgb_image.crop(
+          (x, y, x + self.block_size, y + self.block_size))
+        block1 = self.rgb_image.crop(
+          (x + self.block_size, y, x + (2 * self.block_size), y + self.block_size))
+
+        hex0 = self._get_wrgbk(block0)
+        hex1 = self._get_wrgbk(block1)
+
+        if (hex0 == 4 or hex1 == 4):
+          break
+
+        hex_num = hex0 + hex1 * 4
+        hex_value = hex(hex_num).replace('0x','')
+        hex_string += hex_value
+        count += 1
+
+    new_base64 = binascii.unhexlify(hex_string)
+
+
 
   def run(self):
     # Open the image.
     self.image = Image.open(self.image_path)
 
     # Re{scale,quality} image.
-    self.rescale(self.width, self.height)
+    self.rescale()
     self.requality(self.quality)
 
-    b64_image = self.encode()
+    self.encode()
+    filename = self.encrypt('password')
 
-    c = Cipher('password')
-    b64encoded = c.encode(b64_image)
-
-    hex_data = binascii.hexlify(base64.b64decode(b64encoded))
-
-    width = self.width
-
-    colors = ['#FFFFFF',  '#FF0000', '#00FF00', '#0000FF'];
-    for i in hex_data:
-      hex_val = int(i, 16)
-      base4_1 = math.floor(hex_val / 4.0)
-      base4_0 = hex_val - (base4_1 * 4)
-      y_coord = math.floor(i / width)
-      x_coord = i - (y_coord * width)
+    self.extract_rgb()
 
 
 def main(argv):
@@ -106,7 +195,7 @@ def main(argv):
   decoded = c1.decode(encoded)
   print decoded
 
-  smni = SeeMeNotImage('kenya.jpg', 100, 100, 100)
+  smni = SeeMeNotImage('maple.jpg', 1, 100, 1)
   smni.start()
 
 if __name__ == '__main__':
