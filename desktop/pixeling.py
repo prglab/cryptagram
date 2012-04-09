2fa1b0a92b30ed2593fecaa1ef3a9c28c7f00903
Matt Tierney <mrtierney@gmail.com>
2012-04-09 08:34:27 -0400

    Debugging module.

---
+++ desktop/pixeling.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+class Pixeling(object):
+  def __init__(self, width, height):
+    self.width = width
+    self.height = height
+
+  def fill_rectangle(coordinate_list):
+    pass
+
+  def get_filled_coords():
+    pass
+
