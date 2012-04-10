1b65bfddc922cc730e6f88eaaa490b84a0b5683e
Matt Tierney <mrtierney@gmail.com>
2012-04-10 11:21:29 -0400

    WIP setup script.

--- desktop/setup.py
+++ desktop/setup.py
@@ -1,19 +1,8 @@
 """
-SeeMeNot setup.py script.
+SeeMeNot Desktop setup.py script.
 """
-
-from setuptools import setup
-
-APP = ['code.py']
-DATA_FILES = []
-OPTIONS = {'argv_emulation': True}
-
 import sys
-
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"],
-                     "excludes": ["tkinter"],
-                     }
+from setuptools import setup
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -21,19 +10,32 @@ base = None
 if sys.platform == "win32":
   base = "Win32GUI"
 
+# Build on Linux or Windows.
 if sys.platform in ['linux2', 'win32']:
   from cx_Freeze import setup, Executable
+  copyDependentFiles = True
+  includes = ['Crypto','gflags','PIL']
+
+  # Dependencies are automatically detected, but it might need fine tuning.
+  build_exe_options = {"packages": ["os"],
+                       "excludes": ["tkinter"],
+                       "includes": includes
+                       }
+
   setup(name = "SeeMeNot",
         version = "0.1",
         description = "SeeMeNot: truly private, shareable photos.",
         options = {"build_exe": build_exe_options},
         executables = [Executable("code.py", base=base)])
 
+# Build on Mac OS X.
+APP = ['code.py']
+DATA_FILES = []
+OPTIONS = {'argv_emulation': True}
 
 if sys.platform == 'darwin':
   setup(
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
-    )
+    setup_requires=['py2app','pycrypto','python-gflags','PIL'])
