1b452ef3343fab28581596f184cccbb9de27d7cc
Matt Tierney <mrtierney@gmail.com>
2012-04-16 11:46:54 -0400

    Flags

--- desktop/NewImage.py
+++ desktop/NewImage.py
@@ -6,73 +6,60 @@
 # the last row will be unrecoverable especially if resizing is involved.
 
 import numpy
+import random
+import sys
+import logging
 from SymbolShape import SymbolShape
 from Codec import Codec
 from PIL import Image
 from ImageCoder import Base64MessageSymbolCoder, Base64SymbolSignalCoder
+import gflags
 
-import random
+FLAGS = gflags.FLAGS
+gflags.DEFINE_float('hw_ratio', 1.33, 'height/width ratio', short_name = 'r')
+gflags.DEFINE_integer('quality', 95,
+                      'quality to save encrypted image in range (0,95]. '\
+                        '100 disables quantization',
+                      short_name = 'q')
+gflags.DEFINE_integer('data_length', 16, 'data size to use', short_name = 'l')
+
+gflags.DEFINE_integer('ecc_n', 128, 'codeword length', short_name = 'n')
+gflags.DEFINE_integer('ecc_k', 64, 'message byte length', short_name = 'k')
+
 def randb64s(n):
   values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
   return [values[i] for i in map(random.randrange,[0]*n,[63]*n)]
 
-ss = SymbolShape([[1, 1, 1, 1, 2, 2, 2, 2],
-                  [1, 1, 1, 1, 2, 2, 2, 2]])
-codec = Codec(ss, 1.33, Base64MessageSymbolCoder(), Base64SymbolSignalCoder())
-length = 190000
-print length
-quality = 50
-data = randb64s(length)
-#print ''.join(data)
-im = codec.encode(data)
-im.save('test.jpg',quality=quality)
-with open('test.jpg') as fh:
-  fh.read()
-  print fh.tell(), fh.tell() / float(length)
+def main(argv):
+  try:
+    argv = FLAGS(argv)  # parse flags
+  except gflags.FlagsError, e:
+    print '%s\nUsage: %s ARGS\n%s' % (e, sys.argv[0], FLAGS)
+    sys.exit(1)
 
-read_back_image = Image.open('test.jpg')
-extracted_data = codec.decode(read_back_image)
-errors = 0
-for i, datum in enumerate(data[:min(len(data), len(extracted_data))]):
-  if datum != extracted_data[i]:
-    errors += 1
-print 'Errors:', errors
-import sys
-sys.exit(1)
+  ss = SymbolShape([[1, 1, 1, 1, 2, 2, 2, 2],
+                    [1, 1, 1, 1, 2, 2, 2, 2]])
+  codec = Codec(ss, FLAGS.hw_ratio, Base64MessageSymbolCoder(),
+                Base64SymbolSignalCoder())
 
-# Calculate specs.
-ni = NewImageDimensions()
-ni.data_len = 13
-ni.symbol_height = 2
-ni.symbol_width = 3
+  length = FLAGS.data_length
+  quality = FLAGS.quality
 
-print ni.get_image_dimensions()
-width, height = ni.get_image_symbol_dimensions()
-print width, height
+  data = randb64s(length)
+  #print ''.join(data)
+  im = codec.encode(data)
+  im.save('test.jpg',quality=quality)
+  with open('test.jpg') as fh:
+    fh.read()
+    print fh.tell(), fh.tell() / float(length)
 
-matrix = numpy.ones((width, height))
-values = numpy.zeros((width, height))
+  read_back_image = Image.open('test.jpg')
+  extracted_data = codec.decode(read_back_image)
+  errors = 0
+  for i, datum in enumerate(data[:min(len(data), len(extracted_data))]):
+    if datum != extracted_data[i]:
+      errors += 1
+  print 'Errors:', errors
 
-# Encode data.
-for i in range(ni.data_len):
-  y_coord = int(i / float(width))
-  x_coord = int(i - (y_coord * width))
-  print i, x_coord, y_coord
-  matrix[x_coord, y_coord] -= 1
-
-  values[x_coord, y_coord] = i
-
-last_x_coord = x_coord
-last_y_coord = y_coord
-print 'Difference', (width-1) - last_x_coord
-
-# Copy previous rows onto last.
-print matrix
-nz = matrix.nonzero()
-assert len(nz) == 2
-for i, entry in enumerate(nz[0]):
-  empty_y_row = nz[1][i]
-  values[entry, empty_y_row] = values[entry, empty_y_row-1]
-  print 'Copy from', entry, empty_y_row-1, 'to', entry, empty_y_row
-
-print values
+if __name__=='__main__':
+  main(sys.argv)
