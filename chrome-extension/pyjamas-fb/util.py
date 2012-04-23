c1e92eefb443b55966d2fdf343b9c62b4f351f2b
Matt Tierney <mrtierney@gmail.com>
2012-04-23 19:27:22 -0400

    Update the pyjamas utils.

--- chrome-extension/pyjamas-fb/util.py
+++ chrome-extension/pyjamas-fb/util.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python
-
+import math
 def bsearch(a, x, lo=0, hi=None):
-  def plus_minus_one(val):
-    return [val-1, val, val+1]
-
   if hi is None:
     hi = len(a)-1
 
   if x < a[lo]: return lo
   if x > a[hi]: return hi
 
-  while lo < hi:
-    mid = int((lo+hi)/2)
+  while lo <= hi:
+    mid = int(math.floor(lo+hi)/2)
     midval = a[mid]
     if midval < x:
       lo = mid+1
+      prev_comparison = -1
     elif midval > x:
-      hi = mid
+      hi = mid-1
+      prev_comparison = 1
     else:
       return mid
 
-  distances = dict()
+  if prev_comparison < 0:
+    option_low = mid
+    option_high = mid + 1
+  else:
+    option_low = mid-1
+    option_high = mid
 
-  _ = []
-  for val in [lo, mid, hi]:
-    for y in plus_minus_one(val):
-      _.append(y)
+  dist_a = x - a[option_low]
+  dist_b = a[option_high] - x
 
-  for idx in set(_):
-    if idx not in range(0, len(a)):
-      continue
-    distances[abs(x - a[idx])] = idx
-  ret_val = distances.get(min(distances.keys()))
-  return ret_val
+  if dist_a < dist_b:
+    return option_low
+  else:
+    return option_high
 
 def average(iterable):
   if len(iterable) == 0:
