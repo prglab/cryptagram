a717dedc276d741691bec3a422bfd47ceebf62fb
Matt Tierney <mrtierney@gmail.com>
2012-04-26 13:08:11 -0400

    Patching latent cipher refactoring bugs.

--- desktop/Cipher/PyV8Cipher.py
+++ desktop/Cipher/PyV8Cipher.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
+import os
 import logging
 import PyV8
 from json import JSONDecoder
 
 class V8Cipher(PyV8.JSClass):
-  sjcljs = 'sjcl.js'
-
   def __init__(self, password):
     self.password = password
     PyV8.JSClass.__init__(self)
+
+    if os.name == 'posix':
+      self.sjcljs = 'Cipher/sjcl.js'
+    elif os.name == 'mac':
+      self.sjcljs = 'sjcl.js'
 
   def encode(self, message):
     env = V8Cipher(self)
