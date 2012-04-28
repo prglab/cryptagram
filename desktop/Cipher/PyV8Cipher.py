5165d42650653406e7f4bad668a2a3423ad97643
Matt Tierney <tierney@cs.nyu.edu>
2012-04-28 15:18:03 -0400

    PyV8 now thread safe.

--- desktop/Cipher/PyV8Cipher.py
+++ desktop/Cipher/PyV8Cipher.py
@@ -2,13 +2,13 @@
 import platform
 import logging
 import PyV8
+import threading
+import time
 from json import JSONDecoder
 
-class V8Cipher(PyV8.JSClass):
-  # NOT THREAD-SAFE. DO NOT USE IN A PYTHON THREAD.
+class V8Cipher(object):
   def __init__(self, password, command_line = False):
     self.password = password
-    PyV8.JSClass.__init__(self)
 
     _platform = platform.system()
     if _platform == 'Linux' or command_line:
@@ -16,23 +16,70 @@ class V8Cipher(PyV8.JSClass):
     elif _platform == 'Darwin':
       self.sjcljs = 'sjcl.js'
 
-  def encode(self, message):
-    logging.info('V8Cipher.encode called.')
-    env = V8Cipher(self)
-    with open(self.sjcljs) as fh:
-      with PyV8.JSIsolate():
-        with PyV8.JSContext(env) as ctxt:
-          ctxt.eval(fh.read())
-          resp = ctxt.eval('sjcl.encrypt("%s", "%s")' % (self.password, message))
-          json = JSONDecoder().decode(resp)
-    return json
+  def _do_code(self, action, message):
+    # Based on the specified action, set the coder.
+    coder_class = PyV8Encode if action == 'encode' else PyV8Decode
 
-  def decode(self, json_str):
-    env = V8Cipher(self)
-    with open(self.sjcljs) as fh:
-      with PyV8.JSIsolate():
-        with PyV8.JSContext(env) as ctxt:
-          ctxt.eval(fh.read())
-          resp = ctxt.eval('sjcl.decrypt("%s", \'%s\')' % \
-                             (self.password, json_str))
-    return resp
+    coder = coder_class(self.password, message, self.sjcljs)
+    coder.run()
+    while True:
+      result = coder.get_result()
+      if result:
+        break
+      time.sleep(0.5)
+    return result
+
+  def encode(self, message):
+    return self._do_code('encode', message)
+
+  def decode(self, message):
+    # Expect message to be a JSON string.
+    return self._do_code('decode', message)
+
+
+class PyV8Encode(threading.Thread):
+  result = None
+
+  def __init__(self, password, message, sjcljs):
+    threading.Thread.__init__(self)
+    self.password = password
+    self.message = message
+    self.sjcljs = sjcljs
+
+  def get_result(self):
+    return self.result
+
+  def run(self):
+    with PyV8.JSIsolate():
+      with PyV8.JSContext() as context:
+        with open(self.sjcljs) as fh:
+          sjcl = fh.read()
+        context.eval(sjcl)
+        resp = context.eval('sjcl.encrypt("%s", "%s");' % (self.password,
+                                                           self.message))
+        self.result = JSONDecoder().decode(resp)
+        del context
+
+class PyV8Decode(threading.Thread):
+  result = None
+
+  def __init__(self, password, message, sjcljs):
+    threading.Thread.__init__(self)
+    self.password = password
+    self.message = message
+    self.sjcljs = sjcljs
+
+  def get_result(self):
+    return self.result
+
+  def run(self):
+    with PyV8.JSIsolate():
+      with PyV8.JSContext() as context:
+        with open(self.sjcljs) as fh:
+          sjcl = fh.read()
+        context.eval(sjcl)
+        resp = ctxt.eval('sjcl.decrypt("%s", \'%s\')' % \
+                           (self.password, self.message))
+        self.result = resp
+        del context
+
