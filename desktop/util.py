e612b4cbd22f0a58e5c728ba0b2e21ad3286514d
Matt Tierney <tierney@cs.nyu.edu>
2012-05-02 10:38:52 -0400

    GUI quittable. Still need to add menu items.

--- desktop/util.py
+++ desktop/util.py
@@ -46,3 +46,8 @@ def sha256hash(to_hash):
   integrity_hash.update(to_hash)
   integrity_check_value = integrity_hash.hexdigest()
   return integrity_check_value
+
+def md5hash(to_hash):
+  hash_func = md5()
+  hash_func.update(to_hash)
+  return hash_func.hexdigest()
