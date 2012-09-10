598012943814d34ceed84cbeb3c0c9074cb3ccd0
Matt Tierney <tierney@cs.nyu.edu>
2012-09-10 09:59:22 -0400

    Minor

--- experimental/kmeans.py
+++ experimental/kmeans.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python
 
 import sys
-import scipy.cluster.vq.kmeans as kmeans
+import scipy.cluster.vq as vq # Will use vq.kmeans.
 import google.protobuf as protobuf
+
+class ColorSpace(object):
+
+  @staticmethod
+  def IsValidColor(color_value):
+    return (color_value >= 0) and (color_value <= 255)
+
+  @staticmethod
+  def RGBtoYCC(red, green, blue):
+    lum = 0.299 * red + 0.587 * green + 0.114 * blue
+    cb = 128 - 0.168736 * red - 0.331264 * green + 0.5 * blue
+    cr = 128 + 0.5 * red - 0.418688 * green - 0.081312 * blue
+    return (lum, cb, cr)
+
+  @staticmethod
+  def GenerateObservations(valid_observations):
+    if (valid_observations != []):
+      return False
+
+    for red in range(0, 256):
+      for green in range(0, 256):
+        for blue in range(0, 256):
+          valid_observations.append(ColorSpace.RGBtoYCC(red, green, blue))
+    return True
+
+  def YCCtoRGB():
+    pass
 
 class KmeansExperiment(object):
   # Maps the image to the "error set," which is information about the
@@ -22,7 +49,9 @@ class KmeansExperiment(object):
     pass
 
 def main(argv):
-  pass
+  observations = []
+  ColorSpace.GenerateObservations(observations)
+  print len(observations)
 
 if __name__=='__main__':
   main(sys.argv)
