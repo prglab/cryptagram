beac64b944a6f5dbf862e3a2a1269d64b16d33c5
David <david@david-System76.(none)>
2012-09-12 15:28:18 -0400

    Added ImageView with preview of image

--- android/CryptogramAndroid/src/org/prglab/cryptogram/MainActivity.java
+++ android/CryptogramAndroid/src/org/prglab/cryptogram/MainActivity.java
@@ -4,20 +4,32 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.Menu;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
+import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	public final String DEBUG_TAG = "Cryptogram Main Activity";
+	private final int GALLERY_IMAGE_CODE = 1;
+	private final int CAMERA_IMAGE_CODE = 2;
+	
 
 	Button buttonTakePhoto;
 	Button buttonSelectPhoto;
 	Button buttonUploadPhoto;
+	
+	ImageView imagePreview;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +39,8 @@ public class MainActivity extends Activity {
         buttonTakePhoto = (Button) findViewById(R.id.button_snap_photo);
         buttonSelectPhoto = (Button) findViewById(R.id.button_select_photo);
         buttonUploadPhoto = (Button) findViewById(R.id.button_upload_photo);
-
+        
+        imagePreview = (ImageView) findViewById(R.id.image_preview);
     }
     
     /**
@@ -37,9 +50,24 @@ public class MainActivity extends Activity {
     public void selectPhoto(View v){
 	    Intent intent = new Intent(Intent.ACTION_PICK,
 		android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-		startActivityForResult(intent, 0);
+		startActivityForResult(intent, GALLERY_IMAGE_CODE);
     }
     
+    public void takePhoto(View v){
+    	return;
+    }
+    
+    /**
+     * Set the image to a result from the gallery
+     * @param data
+     */
+    void useImageFromGallery(Intent data){
+    	Uri targetUri = data.getData();
+	   	// Just show the uri in a Toast for now, we'll do something with it later
+	   	Toast.makeText(this, targetUri.toString(), Toast.LENGTH_SHORT).show();
+	
+	   	imagePreview.setImageURI(targetUri);
+    }
     
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -47,10 +75,15 @@ public class MainActivity extends Activity {
 	     super.onActivityResult(requestCode, resultCode, data);
 	
 	     if (resultCode == RESULT_OK){
-	    	 Uri targetUri = data.getData();
-	    	 // Just show the uri in a Toast for now, we'll do something with it later
-	    	 Toast.makeText(this, targetUri.toString(), Toast.LENGTH_SHORT).show();
-	      
+	    	 switch (requestCode){
+	    	 	case GALLERY_IMAGE_CODE:
+	    	 		useImageFromGallery(data);
+	    	 		break;
+	    		
+	    	 	case CAMERA_IMAGE_CODE:
+	    	 		break;
+	    	 }
+	    	 	      
 	     }
     }
 
