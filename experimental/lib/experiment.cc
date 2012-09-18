d297e17e133a342c5165fea908bdf25a7f84b6d9
Matt Tierney <mrtierney@gmail.com>
2012-09-18 17:40:30 -0400

    WIP

---
+++ experimental/lib/experiment.cc
@@ -0,0 +1,15 @@
+/*
+ * experiment.cc
+ *
+ *  Created on: Sep 18, 2012
+ *      Author: tierney
+ */
+
+#include "experiment.h"
+
+namespace cryptogram {
+
+Experiment::Experiment() {
+}
+
+} // namespace cryptogram
