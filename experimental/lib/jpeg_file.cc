1489b14dd8d3a13aa3559b2710984dd0962fc1fc
Matt Tierney <tierney@cs.nyu.edu>
2012-09-19 09:16:18 -0400

    Trials in cpp code working. 29K per sec per core.

--- experimental/lib/jpeg_file.cc
+++ experimental/lib/jpeg_file.cc
@@ -11,26 +11,26 @@
 
 namespace cryptogram {
 
-JpegFile::JpegFile(const std::string& filename,
-                   const unsigned char& array,
-                   int width,
-                   int height,
-                   int quality)
-    : filename_(filename), array_(array), width_(width), height_(height),
-      quality_(quality) {
-}
+// JpegFile::JpegFile(const std::string& filename,
+//                    const unsigned char& array,
+//                    int width,
+//                    int height,
+//                    int quality)
+//     : filename_(filename), array_(array), width_(width), height_(height),
+//       quality_(quality) {
+// }
 
-JpegFile::~JpegFile() {
-}
+// JpegFile::~JpegFile() {
+// }
 
-bool JpegFile::EncodeToFile() {
-  return gfx::JPEGCodec::Encode(array_,
-                                gfx::JPEGCodec::FORMAT_RGB,
-                                width_,
-                                height_,
-                                width_ * cryptogram::kBytesPerWidthEntry,
-                                quality_,
-                                &output);
-}
+// bool JpegFile::EncodeToFile() {
+//   return gfx::JPEGCodec::Encode(array_,
+//                                 gfx::JPEGCodec::FORMAT_RGB,
+//                                 width_,
+//                                 height_,
+//                                 width_ * cryptogram::kBytesPerWidthEntry,
+//                                 quality_,
+//                                 &output);
+// }
 
 } // namespace cryptogram
--- experimental/lib/util.h
+++ experimental/lib/util.h
@@ -1,9 +1,24 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+#include <vector>
+
+#include "glog/logging.h"
+
 namespace cryptogram {
 
 const int kBytesPerWidthEntry = 3;      // Byte per R, G, B.
+
+inline bool UcharVectorToArray(const std::vector<unsigned char>& input,
+                               const int output_capacity,
+                               unsigned char* output) {
+  CHECK_EQ(output_capacity, input.size());
+
+  for (int i = 0; i < output_capacity; i++) {
+    output[i] = input[i];
+  }
+  return true;
+}
 
 } // namespace cryptogram
 
