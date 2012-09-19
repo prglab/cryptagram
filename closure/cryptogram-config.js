d5129a3985b037a96a1bfa63ce880feb66d7c8a4
Ian Spiro <ispiro@gmail.com>
2012-09-19 17:00:54 -0400

    Rough encryption drag n drop demo

--- closure/cryptogram-config.js
+++ closure/cryptogram-config.js
@@ -1,10 +1,10 @@
 {
   "id": "cryptogram",
-  "inputs": ["src/cryptogram/demo.js","src/index.soy","src/sjcl.js"],
+  "inputs": ["src/cryptogram/demo.js","src/index.soy","src/sjcl.js","src/jszip.js","src/downloadify.js","src/swfobject.js"],
   "css-inputs": ["src/style.gss"],
   "css-output-file": "style.css",
   "paths": ["src"],
-  "externs": ["src/sjcl-externs.js"],
+  "externs": ["src/sjcl-externs.js", "src/downloadify-externs.js", "src/swfobject-externs.js"],
   "mode": "advanced",
   "jsdoc-html-output-path": "docs"
 }
