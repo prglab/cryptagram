51e3ed34e4fee02fab63fa8ce5bc554f276c0f16
Ian Spiro <ispiro@gmail.com>
2012-09-26 17:45:04 -0400

    Renamed externs files for consistency

--- closure/cryptogram-background-config.js
+++ closure/cryptogram-background-config.js
@@ -3,7 +3,7 @@
   "inputs": [ "src/cryptogram/extension.js",
               "src/thirdparty/sjcl.js"],
   "paths": ["src"],
-  "externs": ["externs/sjcl_externs.js", 
+  "externs": ["externs/sjcl.js", 
               "externs/chrome_extensions.js"],
   "mode": "advanced"
 }
--- closure/cryptogram-demo-config.js
+++ closure/cryptogram-demo-config.js
@@ -9,9 +9,9 @@
   "css-inputs": ["gss/style.gss"],
   "css-output-file": "build/demo/style.css",
   "paths": ["src", "soy"],
-  "externs": ["externs/sjcl_externs.js", 
-              "externs/downloadify_externs.js", 
-              "externs/swfobject_externs.js",
+  "externs": ["externs/sjcl.js", 
+              "externs/downloadify.js", 
+              "externs/swfobject.js",
               "externs/chrome_extensions.js"],
   "mode": "advanced",
   "jsdoc-html-output-path": "docs"
