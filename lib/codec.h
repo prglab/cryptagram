4eae31f33c942ac0a21fe7bbddc12cb68211c0f4
Matt Tierney <mrtierney@gmail.com>
2012-10-10 16:41:59 -0400

    WIP

--- lib/codec.h
+++ lib/codec.h
@@ -1,7 +1,20 @@
 // Copyright 2012 NYU. All Rights Reserved.
 // Use of this source code is governed by a BSD-style license.
 
+#include "google/logging.h"
+
 namespace prg {
+namespace cryptogram {
 
+class Codec {
+ public:
+  bool Encode();
 
+  bool Decode();
+
+ private:
+  DISALLOW_EVIL_CONSTRUCTORS(Codec);
+};
+
+} // namespace cryptogram
 } // namespace prg
