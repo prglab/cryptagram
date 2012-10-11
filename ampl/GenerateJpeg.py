2aa2ee4f7cc02a44f3e9d5beb9fc6527ffbee8af
Matt Tierney <tierney@cs.nyu.edu>
2012-10-11 17:40:31 -0400

    Generating successive entries.

--- ampl/GenerateJpeg.py
+++ ampl/GenerateJpeg.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+
 from dct import QuantizationTableFromQuality
+from math import floor
 import numpy
 import operator
+import random
 import re
 import subprocess
 
@@ -43,20 +46,20 @@ class Model(object):
   def SetObjective(self, objective):
     self._AppendToModel(objective)
 
-  def Run(self):
+  def Run(self, id):
     # Write out the model and execute.
     with open('temp.mod', 'w') as fh:
       fh.write(self.base + '\nend;\n')
 
-    proc = subprocess.Popen("glpsol -m temp.mod -d jpeg.dat -o temp.sol",
-                            shell=True, stdout=subprocess.PIPE)
+    proc = subprocess.Popen("glpsol -m temp.mod -d jpeg.dat -o ID_%d.sol" % id,
+                            shell=True,
+                            stdout=subprocess.PIPE)
     proc.communicate()
-    return self._ParseForObjective()
+    return self._ParseForObjective(id)
 
-  def _ParseForObjective(self):
-    # Objective:  z = 942.3571232 (MAXimum)
+  def _ParseForObjective(self, id):
     results = ''
-    with open('temp.sol', 'r') as fh:
+    with open('ID_%d.sol' % id, 'r') as fh:
       for result in fh.readlines():
         m = re.match(kReObjectiveFunction, result)
         if m:
@@ -65,19 +68,63 @@ class Model(object):
 def main():
   objective_results = {}
 
-  for i in range(0,8):
-    for j in range(0,8):
-      model = Model(model_base)
-      model.SetObjective('maximize z: F[%d,%d];' % (i,j))
-      model.AddConstraint(kDctConstraint)
-      model.AddConstraint(kNdctConstraint)
-      objective_results[(i,j)] = model.Run()
+  coeffs_order = []
+  constraints = {}
+  try:
+    for k in range(0,20):
+      for i in range(0,8):
+        for j in range(0,8):
+          model = None
+          model = Model(model_base)
+          model.SetObjective('maximize z: F[%d,%d];' % (i,j))
+          model.AddConstraint(kDctConstraint)
+          model.AddConstraint(kNdctConstraint)
+          for constraint_i, constraint in enumerate(constraints):
+            coeff_h, coeff_v = constraint
+            model.AddConstraint("FIX_%d_%d: F[%d,%d] = %d;" % \
+                                (coeff_h,
+                                 coeff_v,
+                                 coeff_h,
+                                 coeff_v,
+                                 constraints[constraint]))
 
-  (coeff_h, coeff_v), max_val = max(objective_results.iteritems(),
-                                    key=operator.itemgetter(1))
+          objective_results[(i,j)] = model.Run(k)
 
-  quantiz_ = quant_table[coeff_h, coeff_v]
-  print max_val
-  print max_val / quantiz_
+      sorted_results = sorted(objective_results.iteritems(),
+                              key=operator.itemgetter(1),
+                              reverse=True)
+
+      for result in sorted_results:
+        coeffs, max_val = result
+        if coeffs in constraints:
+          continue
+
+        (coeff_h, coeff_v) = coeffs
+
+        quantiz_ = int(quant_table[coeff_h, coeff_v])
+        coeffs_order.append(coeffs)
+        print coeffs, max_val
+
+        # Joined discretization values.
+        joined_values = range(0, int(floor(max_val)), quantiz_) + \
+                        range(-quantiz_, -int(floor(max_val)), -quantiz_)
+
+        constraints[(coeff_h, coeff_v)] = random.choice(joined_values)
+        break
+
+  except IndexError:
+    with open('ID_%d.sol' % (k - 1)) as fh:
+      lines = fh.readlines()
+    print coeffs_order
+    for line in lines:
+      if 'FIX' not in line:
+        continue
+
+      sline = line.split()
+      fix_h_v = sline[1]
+      coeff_h, coeff_v = fix_h_v.split('_')[1:]
+      print coeff_h, coeff_v, sline[3]
+    return
+
 
 main()
