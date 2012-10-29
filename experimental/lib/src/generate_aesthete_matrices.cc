72362fbabec9fe154988dd2dbc669388cc372efb
Matt Tierney <tierney@cs.nyu.edu>
2012-10-29 16:53:35 -0400

    WIP

--- experimental/lib/src/generate_aesthete_matrices.cc
+++ experimental/lib/src/generate_aesthete_matrices.cc
@@ -1,11 +1,13 @@
 // Copyright 2012. The Cryptogram Authors. BSD License.
 // Author: tierney@cs.nyu.edu (Matt Tierney)
 
+#include <cstdlib>
+
 #include "aesthete.h"
 
 int main(int argc, char** argv) {
   srand(0);
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 10000000; i++) {
     cryptogram::MatrixRepresentation mr;
     vector<int> discretizations;
     for (int j = 0; j < 16; j++) {
