2a2c203f593f53359048f652bbd298d57707ae39
David <david.iserovich@gmail.com>
2012-10-29 23:05:05 -0400

    Added context menu entry, basic javascript hooks
    There are now checkboxes in the cryptogram menu entry

--- extension/firefox/chrome/content/cryptogram.js
+++ extension/firefox/chrome/content/cryptogram.js
@@ -1,6 +1,42 @@
 var cryptogram = {
-  run: function() {
-    alert("Cryptogram is running");
-  }
+    dbgtag: "cryptogram : ",
+  
+    run: function() {
+      alert("Cryptogram is running");
+    },
+  
+    init: function() {
+      var contextMenu = document.getElementById("contentAreaContextMenu");
+        if (contextMenu){
+            contextMenu.addEventListener("popupshowing", this.showCryptogramMenuEntry, false);
+        }
+    },
+  
+    showCryptogramMenuEntry: function (event)
+    {
+      var show = document.getElementById("cryptogram-context-decrypt");
+      show.hidden = !(gContextMenu.onImage);
+    },
+    
+    decrypt: function () {
+        alert("Decrypt button pressed");
+        var img = gContextMenu.target;
+        if (img !== undefined){
+          if (img.src !== undefined){
+            img.src = "fake";
+            alert("decrypt button pressed");
+          }
+          // Debug
+          else {
+              alert("Decrypt image is being shown in a nonsense context");         
+          }
+        }
+    }
 
 }
+
+
+
+
+
+window.addEventListener("load", function() { cryptogram.init(); }, false);
