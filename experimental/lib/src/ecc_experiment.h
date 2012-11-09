63d70029ad515f1812328900477663073e7a3af3
Matt Tierney <mrtierney@gmail.com>
2012-11-09 18:44:46 -0500

    WIP

---
+++ experimental/lib/src/ecc_experiment.h
@@ -0,0 +1,16 @@
+#ifndef _ECC_EXPERIMENT_H_
+#define _ECC_EXPERIMENT_H_
+
+namespace cryptogram {
+
+class EccExperiment {
+ public:
+
+  
+ private:
+};
+
+
+} // namespace cryptogram
+
+#endif  // _ECC_EXPERIMENT_H_
---
+++ experimental/lib/src/ecc_thread.h
@@ -0,0 +1,10 @@
+#ifndef _ECC_THREAD_H_
+#define _ECC_THREAD_H_
+
+namespace cryptogram {
+
+
+
+} // namespace cryptogram
+
+#endif  // _ECC_THREAD_H_
