b547d6343de4d94299b0291c87bfd302a6843e53
Matt Tierney <mrtierney@gmail.com>
2012-11-10 15:22:20 -0500

    Adding ecc_thread implementation.

---
+++ experimental/lib/src/ecc_thread.cc
@@ -0,0 +1,41 @@
+#include "ecc_thread.h"
+
+#include <fstream>
+#include <iostream>
+#include <pthread.h>
+
+#include "ecc_experiment.h"
+#include "glog/logging.h"
+
+namespace cryptogram {
+
+EccThread::EccThread(int id, int iterations)
+    : id_(id), iterations_(iterations) {
+}
+
+EccThread::~EccThread() {
+}
+
+void EccThread::Start() {
+  CHECK_EQ(pthread_create(&thread_, NULL, &EccThread::Run, this), 0);
+}
+
+void EccThread::Join() {
+  CHECK_EQ(pthread_join(thread_, NULL), 0);
+}
+
+void* EccThread::Run(void* context) {
+  EccThread* self = static_cast<EccThread*>(context);
+
+  std::ostringstream f_str_stream;
+  f_str_stream << "ecc_out_" << self->iterations_ << "_" << self->id_ << ".txt";
+  
+  EccExperiment experiment(f_str_stream.str());
+  for (int iteration = 0; iteration < self->iterations_; iteration++) {
+    experiment.Run();
+  }
+
+  return NULL;
+}
+
+} // namespace cryptogram
