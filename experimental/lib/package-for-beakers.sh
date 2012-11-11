9d7ba639edd662ede6f0a70649c627d9698681c7
Matt Tierney <tierney@cs.nyu.edu>
2012-11-10 23:24:49 -0500

    Making sure all the relevant data is printed.

--- experimental/lib/package-for-beakers.sh
+++ experimental/lib/package-for-beakers.sh
@@ -3,3 +3,5 @@
 rm cryptogram-1.0.tar.gz
 make distcheck DISTCHECK_CONFIGURE_FLAGS=--with-tcmalloc DIST_CONFIGURE_FLAGS=--with-tcmalloc -j4
 cp cryptogram-1.0.tar.gz /nfs
+echo
+echo "Packaged and ready to go at /nfs"
