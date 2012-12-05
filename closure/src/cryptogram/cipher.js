9871a8f7a44ba232ee1727867aab38bb5a05894d
Matt Tierney <tierney@cs.nyu.edu>
2012-12-05 12:21:50 -0500

    Basic remote logger for closure

--- closure/src/cryptogram/cipher.js
+++ closure/src/cryptogram/cipher.js
@@ -1,7 +1,8 @@
 goog.provide('cryptogram.cipher');
 
-goog.require('goog.debug.Logger');
+goog.require('cryptogram.logger');
 
+goog.require('goog.debug.Logger');
 
 /**
  * @constructor
@@ -9,15 +10,16 @@ goog.require('goog.debug.Logger');
 cryptogram.cipher = function() {
 };
 
-cryptogram.cipher.prototype.logger = goog.debug.Logger.getLogger('cryptogram.cipher');
+// cryptogram.cipher.prototype.logger = goog.debug.Logger.getLogger('cryptogram.cipher');
 
+cryptogram.cipher.prototype.logger = new cryptogram.logger();
 
 cryptogram.cipher.URIHeader = "data:image/jpeg;base64,";
 
-/** 
+/**
  */
 cryptogram.cipher.prototype.decrypt = function(newBase64, password) {
-  
+
   var check = newBase64.substring(0,64);
   var iv = newBase64.substring(64,86);
   var salt = newBase64.substring(86,97);
@@ -25,32 +27,30 @@ cryptogram.cipher.prototype.decrypt = function(newBase64, password) {
   var full = newBase64.substring(64,newBase64.length);
   var bits = sjcl.hash.sha256.hash(full);
   var hexHash = sjcl.codec.hex.fromBits(bits);
-  
+
   if (hexHash != check) {
     this.logger.severe("Checksum failed. Image is corrupted.");
     return;
   } else {
     this.logger.info("Checksum passed.");
   }
-    
+
   var obj = new Object();
   obj['iv'] = iv;
   obj['salt'] = salt;
   obj['ct'] = ct;
   var base64Decode = JSON.stringify(obj);
   var decrypted;
-  
+
   try {
     decrypted = sjcl.decrypt(password, base64Decode);
-  } 
-  
-  catch(err) {
+  } catch(err) {
     this.logger.severe("Error decrypting:" + err.toString());
     return;
   }
-  
+
   this.logger.info("Decrypted " + decrypted.length + " base64 characters.");
-  
+
   var payload = cryptogram.cipher.URIHeader + decrypted;
   return payload;
 };
@@ -59,12 +59,12 @@ cryptogram.cipher.prototype.encrypt = function(data, password) {
   // Get rid of data type information (for now assuming always JPEG.
   var withoutMimeHeader = data.split('base64,')[1];
   var encrypted_data = JSON.parse(sjcl.encrypt(password, withoutMimeHeader));
-    
+
   var iv = encrypted_data['iv'];
   var salt = encrypted_data['salt'];
   var ct = encrypted_data['ct'];
   var to_hash = iv + salt + ct;
 	var bits = sjcl.hash.sha256.hash(to_hash);
-  var integrity_check_value = sjcl.codec.hex.fromBits(bits);  
+  var integrity_check_value = sjcl.codec.hex.fromBits(bits);
   return integrity_check_value + to_hash;
 };
