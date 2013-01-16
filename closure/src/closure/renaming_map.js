0987468e72f5ed6a275bc53c217b7358c95f0e37
Matt Tierney <mrtierney@gmail.com>
2013-01-16 16:56:15 -0500

    WIP testing the requality

--- closure/src/closure/renaming_map.js
+++ closure/src/closure/renaming_map.js
@@ -1,4 +1,14 @@
 goog.setCssNameMapping({
-  "status": "a",
-  "frame": "b"
+  "goog": "a",
+  "inline": "b",
+  "block": "c",
+  "dialog": "e",
+  "modal": "d",
+  "bg": "f",
+  "title": "g",
+  "close": "h",
+  "buttons": "i",
+  "content": "j",
+  "default": "l",
+  "buttonset": "k"
 });
