0cfd16a838d41d8f4d273aab604be3505d81b7ac
Matt Tierney <tierney@cs.nyu.edu>
2013-01-18 15:14:02 -0500

    Reducers in place. Seems to work well. Need a better model though.

--- closure/src/cryptagram/encoder.js
+++ closure/src/cryptagram/encoder.js
@@ -19,12 +19,14 @@ goog.require('cryptagram.cipher');
 goog.require('cryptagram.codec.bacchant');
 goog.require('cryptagram.loader');
 goog.require('cryptagram.RemoteLog');
-goog.require('cryptagram.Requality');
-goog.require('cryptagram.Requality.Event');
-goog.require('cryptagram.Requality.EventType');
-goog.require('cryptagram.ResizeValidator');
-goog.require('cryptagram.ResizeValidator.Event');
-goog.require('cryptagram.ResizeValidator.EventType');
+
+goog.require('cryptagram.SizeReducer');
+goog.require('cryptagram.SizeReducer.Event');
+goog.require('cryptagram.SizeReducer.EventType');
+
+goog.require('cryptagram.ReductionEstimator');
+goog.require('cryptagram.ReductionEstimator.Event');
+goog.require('cryptagram.ReductionEstimator.EventType');
 
 /**
  * This class demonstrates some of the core functionality of cryptagram.
@@ -41,10 +43,6 @@ cryptagram.encoder.prototype.logger =
 cryptagram.encoder.prototype.setStatus = function(message) {
   console.log(message);
 };
-
-// Stubbed.
-cryptagram.encoder.prototype.reduceSize = function(image, fraction) {
-}
 
 /** @enum {string} */
 cryptagram.encoder.EventType = {
@@ -67,41 +65,39 @@ goog.inherits(cryptagram.encoder.EncoderEventTarget, goog.events.EventTarget);
 
 cryptagram.encoder.prototype.readerOnload = function (loadEvent) {
   var self = this;
-  var originalData = loadEvent.target.result;
+  var originalImgDataUrl = loadEvent.target.result;
 
   // TODO(tierney): Expose this parameter for other programmers.
   var newQuality = 0.8;
 
   // Setup the requality engine event though we may not use it.
-  var requality = new cryptagram.Requality();
-  var requalityListenKey = goog.events.listen(
-    requality,
-    "REQUALITY_DONE",
+  var sizeReducer = new cryptagram.SizeReducer();
+  var sizeReducerListenKey = goog.events.listen(
+    sizeReducer,
+    "SIZE_REDUCER_DONE",
     function (event) {
-      console.log("Got this from the requality: " + event.image.length + " "
+      console.log("Got this from the sizeReducer: " + event.image.length + " "
                   + event.image.substring(0,100));
       self.encodeImage(event.image);
     },
     true,
     this);
 
-  // Decide whether we should reduce the quality of the image or not.
-  var resizer = new cryptagram.ResizeValidator();
+  // Get from model the size reduction that we need.
+  var reductionEstimator = new cryptagram.ReductionEstimator();
   goog.events.listen(
-    resizer,
-    "RESIZE_VALIDATION",
+    reductionEstimator,
+    "REDUCTION_ESTIMATOR_DONE",
     function (event) {
-      if (event.valid) {
-        console.log("Reducing quality.");
-        requality.start(originalData, newQuality);
+      if (event.fraction < 1.0) {
+        sizeReducer.start(originalImgDataUrl, event.fraction, newQuality);
       } else {
-        goog.events.unlistenByKey(requalityListenKey);
-        this.encodeImage(originalData);
+        self.encodeImage(originalImgDataUrl);
       }
     },
     true,
     this);
-  resizer.validate(2048, 2048, originalData);
+  reductionEstimator.getEstimate(originalImgDataUrl, newQuality);
 };
 
 cryptagram.encoder.prototype.encodeImage = function (dataToEncode) {
