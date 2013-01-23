7b0943ec568be879119e871c4699bfa9522eeb99
Ian Spiro <ispiro@gmail.com>
2013-01-23 11:50:55 -0500

    Separate build and serve demo setups

--- closure/serve_demo.sh
+++ closure/serve_demo.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Need to delete the renaming map for CSS to work
-echo > src/closure/renaming_map.js
-
 # Serve with plovr
-java -jar ../../plovr/build/plovr.jar serve cryptagram-demo-config.js
+java -jar ../../plovr/build/plovr.jar serve cryptagram-demo-dev-config.js
