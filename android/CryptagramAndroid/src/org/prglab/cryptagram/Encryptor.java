79e30af48f4c63e90f160eec1c8b366cce0a065e
David <david.iserovich@gmail.com>
2013-02-22 13:04:28 -0500

    Renamed Android vers. to Cryptagram. Added unit test project for codec. Moved up to Android 4.0 minimum, changed styling

---
+++ android/CryptagramAndroid/src/org/prglab/cryptagram/Encryptor.java
@@ -0,0 +1,5 @@
+package org.prglab.cryptagram;
+
+public class Encryptor {
+	
+}
---
+++ android/CryptagramAndroid/src/org/prglab/cryptagram/ImageDecoder.java
@@ -0,0 +1,15 @@
+package org.prglab.cryptagram;
+
+import android.graphics.Bitmap;
+
+public interface ImageDecoder {
+	/**
+	 * Try decoding the given bitmap with the given password using a
+	 * Cryptagram decoding scheme
+	 * 
+	 * @param encryptedImage the Bitmap of the image to decode
+	 * @param password the password protecting the image
+	 * @return the base64-encoded data in the image
+	 */
+	public abstract String decodeBitmap(Bitmap encryptedImage, String password);
+}
