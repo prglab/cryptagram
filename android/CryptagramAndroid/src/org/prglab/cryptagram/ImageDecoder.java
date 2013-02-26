8ae24b84bf25d7ed95e51963b99a7dfdb8351687
David <david.iserovich@gmail.com>
2013-02-25 19:32:17 -0500

    Codec fixes, working on passing unit tests

--- android/CryptagramAndroid/src/org/prglab/cryptagram/ImageDecoder.java
+++ android/CryptagramAndroid/src/org/prglab/cryptagram/ImageDecoder.java
@@ -3,6 +3,11 @@ package org.prglab.cryptagram;
 import android.graphics.Bitmap;
 
 public interface ImageDecoder {
+	
+	class HashCheckFailedException extends Exception{
+		public static final long serialVersionUID = 1L;
+	}
+	
 	/**
 	 * Try decoding the given bitmap with the given password using a
 	 * Cryptagram decoding scheme
@@ -10,5 +15,5 @@ public interface ImageDecoder {
 	 * @param encodedImage the Bitmap of the image to decode
 	 * @return the base64-encoded data in the image
 	 */
-	public abstract String decodeBitmap(Bitmap encodedImage);
+	public abstract String decodeBitmap(Bitmap encodedImage) throws HashCheckFailedException;
 }
